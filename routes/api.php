833017b4bad5a58eaf5c45f47d26647fdafa06ed
Dang Huy Hoang <doiquanhieumk@gmail.com>
2024-07-14 22:26:25 +0700

    Disable the API v1

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__ . '/api/v1.php';
+// require __DIR__ . '/api/v1.php';
 require __DIR__ . '/api/v2.php';
 
 
